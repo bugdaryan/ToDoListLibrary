d0034edb8b2ddc3c0d6f60ced9f01c388d35b702
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-03-31 19:41:18 +0400

    Add constructor to ToDoList Add indexer Add private ToDoItem array

--- ToDoListLibrary/ToDoItem.cs
+++ ToDoListLibrary/ToDoItem.cs
@@ -2,6 +2,7 @@
 {
     public class ToDoItem
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public bool IsDone { get; set; }
--- ToDoListLibrary/ToDoList.cs
+++ ToDoListLibrary/ToDoList.cs
@@ -1,11 +1,90 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 namespace ToDoListLibrary
 {
     public class ToDoList:IEnumerable<ToDoItem>
     {
+        #region Public properties
+
+        public int Count => _count;
+        
+
+        #endregion
+
+
+        #region Private members
+
+        private readonly ToDoItem[] _todos;
+        private int _count;
+
+        #endregion
+
+        #region Constructors
+        public ToDoList(IEnumerable<ToDoItem> todos)
+        {
+            _todos = todos.ToArray();
+        }
+
+        public ToDoList(int count)
+        {
+            _todos = new ToDoItem[count];
+            _count = count;
+        }
+
+
+        #endregion
+
+        #region Overrides
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public ToDoItem this[int index]
+        {
+            get
+            {
+                CheckIndexBounds(index);
+                return _todos[index];
+            }
+            set
+            {
+                CheckIndexBounds(index);
+                _todos[index] = value;
+            }
+        }
+
+        #endregion
+
+        #region Private functions
+
+        public void CheckIndexBounds(int index)
+        {
+            if (index < 0 || index >= _count)
+            {
+                throw new IndexOutOfRangeException("Index is outside of bounds");
+            }
+        }
+
+        #endregion
+
+        #region IEnumerable
+
         public IEnumerator<ToDoItem> GetEnumerator()
         {
             throw new NotImplementedException();
@@ -15,5 +94,8 @@ namespace ToDoListLibrary
         {
             return GetEnumerator();
         }
+
+
+        #endregion
     }
 }
